431cc7c7c9b8ebe9792e58aebe74f7f700679685
Matt Ackard <mattackard@gmail.com>
2018-12-05 21:08:55 -0800

    placeholder only changes when new todo is submitted

--- src/components/TextInput.js
+++ src/components/TextInput.js
@@ -1,26 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+
+let placeholders = [
+    'Buy milk',
+    'Run a mile',
+    'Do some laundry',
+    'Pay water bill'
+];
+
+//scoped outside TextInput to avoid refreshing currentPlaceholder value everytime the state is updated in the controlled input
+let currentPlaceholder = placeholders[Math.floor(Math.random() * placeholders.length)];
+
 const TextInput = ({submitTodo}) => {
 
     let todo = React.createRef();
 
-    let placeHolders = [
-        'Buy milk',
-        'Run a mile',
-        'Do some laundry',
-        'Pay water bill'
-    ];
+    let changePlaceholder = () => {
+        return placeholders[Math.floor(Math.random() * placeholders.length)];
+    }
 
     let handleSubmit = (e) => {
         e.preventDefault();
         submitTodo(todo.current.value);
+        currentPlaceholder = changePlaceholder();
         e.currentTarget.reset();
     }
 
     return (
         <form className="textInput" onSubmit={handleSubmit}>
-            <input type="text" ref={todo} placeholder={placeHolders[Math.floor(Math.random() * placeHolders.length)]} />
+            <input type="text" ref={todo} placeholder={currentPlaceholder} />
             <button type="submit">Submit</button>
         </form>
     );
