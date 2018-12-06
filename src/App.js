275036fdc405384ee7b678dc3cb6df33b7659906
Matthew Ackard <mattackard@gmail.com>
2018-12-06 15:55:28 -0800

    switched css to sass

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.scss';
 
 import TodoItem from './components/TodoItem';
 import TextInput from './components/TextInput';
