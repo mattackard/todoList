14c999879e51077a6881d743a6ff842ca306054a
Matt Ackard <mattackard@gmail.com>
2019-01-09 22:38:13 -0800

    Updates

---
+++ precache-manifest.08c28a9f4048e4830547d77621a79851.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a695b30138abc35248a",
+    "url": "/todo/static/js/runtime~main.8a695b30.js"
+  },
+  {
+    "revision": "503486148eb6da50fa1e",
+    "url": "/todo/static/js/main.50348614.chunk.js"
+  },
+  {
+    "revision": "6d1635c7e8872c75e39f",
+    "url": "/todo/static/js/1.6d1635c7.chunk.js"
+  },
+  {
+    "revision": "503486148eb6da50fa1e",
+    "url": "/todo/static/css/main.9acc5759.chunk.css"
+  },
+  {
+    "revision": "72d3ba12b054bce50249bc23df9ebd96",
+    "url": "/todo/index.html"
+  }
+];
